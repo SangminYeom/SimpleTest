0e59b668a041dcd733f943ddfd3f15dd65b29cb1
Sangmin Yeom <sangminyeom@gmail.com>
2021-11-21 16:37:05 +0900

    Initial Commit

---
+++ Shared/SimpleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTestApp.swift
+//  Shared
+//
+//  Created by SANGMIN YEOM on 2021/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
